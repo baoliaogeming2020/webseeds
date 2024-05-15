16e98a39babb8ac612c07165f044cd679268b465
baoliaogeming2020 <68335264+baoliaogeming2020@users.noreply.github.com>
2024-05-15 20:15:47 +0800

    Add files via upload

--- example.js
+++ example.js
@@ -1,7 +1,12 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    executablePath: process.env.PUPPETEER_EXEC_PATH,
+    headless: false,
+    ...
+});
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({ path: 'example.png' });
