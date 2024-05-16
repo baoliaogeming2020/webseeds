3d3becfe12b0e2c4f25890b6b0b22bab27902607
baoliaogeming2020 <68335264+baoliaogeming2020@users.noreply.github.com>
2024-05-16 13:33:14 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,9 +27,9 @@ line = f.readline()
 
 while line:
     print(line)
-    driver.implicitly_wait(10)
+    #driver.implicitly_wait(10)
     driver.get(line)
-    #sleep(5)
+    sleep(5)
     line = f.readline()
 
 f.close()
