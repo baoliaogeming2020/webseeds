79d9e9b84ec2acfb30ce6121892f945d7fba4ac1
baoliaogeming2020 <68335264+baoliaogeming2020@users.noreply.github.com>
2024-05-16 12:34:40 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,9 +19,14 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-for line in open("link.txt", "r")
-		print(line)
-fo.close()
+f = open("link.txt")               # 返回一个文件对象 
+line = f.readline()               # 调用文件的 readline()方法 
+while line: 
+    #print line,                   # 后面跟 ',' 将忽略换行符 
+    print(line, end = '')　      # 在 Python 3 中使用 
+    line = f.readline() 
+ 
+f.close()  
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
